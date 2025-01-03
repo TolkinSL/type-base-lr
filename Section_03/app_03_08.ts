1bfb63074aaf261a85cc693a9c1879fb2478baef
TolkinSL <tolkinsl@yandex.ru>
2025-01-03 04:14:25 +0300

    SC_03_09 ReadOnly типы

--- Section_03/app_03_08.ts
+++ Section_03/app_03_08.ts
@@ -6,4 +6,10 @@ console.log(mySkills);
 const [id, skillName] = mySkills;
 console.log(id + ' ' + skillName);
 
-const arrSKills: [number, string, ...boolean[]] = [1, 'DevOps', true, false];
+const arrSkills: [number, string, ...boolean[]] = [1, 'DevOps', true, false];
+
+const readonlyTuple: readonly [number, string] = [1, 'Php'];
+// readonlyTuple[0] = 2;
+// readonlyTuple.push('Azz');
+
+const readonlyArr1: ReadonlyArray<string> = ['1a', '2b'];
