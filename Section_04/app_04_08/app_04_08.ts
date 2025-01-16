40b2b2f67af50a03f0ad39fda5add6cc5212a04e
TolkinSL <tolkinsl@yandex.ru>
2025-01-16 08:42:37 +0300

    SC_04_08q Ответ сервера и сопоставление с Enum

---
+++ Section_04/app_04_08/app_04_08.ts
@@ -0,0 +1,66 @@
+//Эмуляция ответа сервера и сопоставление с Enum
+
+enum PaymentStatus {
+  Success = 'success',
+  Failed = 'failed',
+}
+
+interface Payment {
+  sum: number;
+  from: number;
+  to: number;
+}
+
+interface SuccessData extends Payment {
+  databaseId: number;
+}
+
+interface FailedData {
+  errorMessage: string;
+  errorCode: number;
+}
+
+interface ResponseSuccess {
+  status: PaymentStatus.Success;
+  data: SuccessData;
+}
+
+interface ResponseFailed {
+  status: PaymentStatus.Failed;
+  data: FailedData;
+}
+
+// Запрос в виде платежа
+const out: Payment = {
+  sum: 10000,
+  from: 2,
+  to: 4,
+};
+
+// Ответ
+const rawIn1 = `{
+    "status": "success",
+    "data": {
+        "databaseId": 567,
+        "sum": 10000,
+        "from": 2,
+        "to": 4
+    }
+}`;
+
+const rawIn2 = `{
+    "status": "failed",
+    "data": {
+        "errorMessage": "Недостаточно средств",
+        "errorCode": 4
+    }
+}`;
+
+const in1: ResponseSuccess | ResponseFailed = JSON.parse(rawIn1);
+const in2: ResponseSuccess | ResponseFailed = JSON.parse(rawIn2);
+//Testing data
+const getData1: ResponseSuccess | ResponseFailed = in1;
+const getData2: ResponseSuccess | ResponseFailed = in2;
+
+console.log(in1.status);
+console.log(in2.status);
