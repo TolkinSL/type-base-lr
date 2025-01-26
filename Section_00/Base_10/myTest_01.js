b6e55eba202454f96994e56c93be8f43df4ad7c6
TolkinSL <tolkinsl@yandex.ru>
2025-01-27 00:38:51 +0300

    JS_10_01 Каллбэки

---
+++ Section_00/Base_10/myTest_01.js
@@ -0,0 +1,8 @@
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+  script.onload = () => callback(script);
+  document.head.append(script);
+}
+
+loadScript('./myTest_01_ext01.js', () => myExtend());
---
+++ Section_00/Base_10/myTest_01_ext01.js
@@ -0,0 +1,5 @@
+console.log('Скрипт загружен');
+
+function myExtend() {
+  console.log('Внешняя функция f_ext01');
+}
